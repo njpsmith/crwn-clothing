9a4cf10c8527d4fd4647bb3b47435ea194dc213c
Nick Smith <>
2020-07-01 18:14:01 +0100

    created user and cart sagas for sign in, sign up and sign in with google

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,16 +1,32 @@
-import { UserActionTypes } from './user.types';
+import UserActionTypes from './user.types';
 
 const INITIAL_STATE = {
   currentUser: null,
+  error: null,
 };
 
 const userReducer = (previousState = INITIAL_STATE, action) => {
   switch (action.type) {
-    case UserActionTypes.SET_CURRENT_USER:
+    case UserActionTypes.SIGN_IN_SUCCESS:
       return {
         ...previousState,
         currentUser: action.payload,
+        error: null,
       };
+    case UserActionTypes.SIGN_OUT_SUCCESS:
+      return {
+        ...previousState,
+        currentUser: null,
+        error: null,
+      };
+    case UserActionTypes.SIGN_IN_FAILURE:
+    case UserActionTypes.SIGN_OUT_FAILURE:
+    case UserActionTypes.SIGN_UP_FAILURE:
+      return {
+        ...previousState,
+        error: action.payload,
+      };
+
     default:
       return previousState;
   }
